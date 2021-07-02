670cb1844eb09200eb86bb7993881cee41a1c7a5
BeGeos <montepeloso.m@gmail.com>
2021-07-02 14:58:52 +0200

    Added CORS module for API

--- server.js
+++ server.js
@@ -2,27 +2,29 @@ const express = require("express");
 const multer = require("multer");
 const path = require("path");
 const morgan = require("morgan");
-let upload = multer({dest: "uploads/"});
+const cors = require("cors");
+let upload = multer({ dest: "uploads/" });
 
-app = express()
-app.use(express.json())
-app.use(morgan("dev"))
+app = express();
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'))
-})
+  res.sendFile(path.join(__dirname, "/index.html"));
+});
 
 app.get("/api/fileanalyse", (req, res) => {
-    res.json({message: "Hello there"})
-})
+  res.json({ message: "Hello there" });
+});
 
 app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
-    // console.log(req.file)
-    res.json({
-        name: req.file.originalname,
-        type: req.file.mimetype,
-        size: req.file.size
-    })
-})
+  // console.log(req.file)
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  });
+});
 
-app.listen(3000, () => console.log("Listening on port 3000..."))
+app.listen(3000, () => console.log("Listening on port 3000..."));
